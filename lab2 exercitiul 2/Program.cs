59e26e82f053ed922cbc027f65d04577b616bb89
ROXANA lazarescu <roxana_lazarescu13@yahoo.com>
2022-05-04 14:58:32 +0300

    Add project files.

---
+++ lab2 exercitiul 2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace lab2_exercitiul_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ex2();
+            //Ex3();
+
+
+
+            Console.ReadLine();
+        }
+        static void Ex2()
+        {
+            //daca numerele sunt pare, atunci imi calculeaza media aritmetica
+            //(2+4+6+8)/4
+            Console.WriteLine("Introduceti numarul n");
+            
+            
+            int n = int.Parse(Console.ReadLine());
+            int nr;
+
+            if (n > 0)
+            {
+                int cateSuntPare = 0;
+                int suma = 0;
+
+                for (int i = 1; i <= n; i++)//
+                {
+                    Console.WriteLine(" Citeste numarul ");
+                    nr = int.Parse(Console.ReadLine());
+
+                    if (nr % 2 == 0)//daca e par
+                    {
+                        suma = suma + nr;//il adaug la suma
+
+                        cateSuntPare = cateSuntPare + 1;//si il si numar
+
+
+                    }
+                }
+
+                decimal mediaAritmetica = 0;
+
+                if (cateSuntPare != 0)
+                    mediaAritmetica = (decimal) suma / cateSuntPare;
+
+                Console.WriteLine("mediaAritmetica=" + mediaAritmetica);
+
+
+                 
+
+
+
+
+
+
+            }
+        }
+    }
+
+}
+
+
+
+        
+
+
+    
+
+
